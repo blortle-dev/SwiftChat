73eaacbb5c62b1b52e66f6e67bb4f6fd0c910143
blortle <144735570+blortle-dev@users.noreply.github.com>
2024-07-02 21:41:00 -0600

    Initial Commit

---
+++ SwiftChat/SwiftChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChatApp.swift
+//  SwiftChat
+//
+//  Created by Colin DiCarlo on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
