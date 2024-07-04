e45d6f38b3d9531120510b7a2516cc16ac6f4504
blortle <144735570+blortle-dev@users.noreply.github.com>
2024-07-03 22:18:53 -0600

    Added files

--- SwiftChat/SwiftChatApp.swift
+++ SwiftChat/SwiftChatApp.swift
@@ -1,17 +1,78 @@
-//
-//  SwiftChatApp.swift
-//  SwiftChat
-//
-//  Created by Colin DiCarlo on 7/2/24.
-//
-
 import SwiftUI
+import WebKit
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem?
+    var popover: NSPopover?
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 400, height: 650)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: WebView(url: URL(string: "https://chat.openai.com/")!))
+        self.popover = popover
+        
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        if let button = statusItem?.button {
+            button.image = NSImage(named: "16")
+            button.action = #selector(togglePopover)
+        }
+    }
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = statusItem?.button {
+            if popover?.isShown == true {
+                popover?.performClose(sender)
+            } else {
+                popover?.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
+    }
+}
+
+struct WebView: NSViewRepresentable {
+    let url: URL
+    
+    func makeNSView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        return webView
+    }
+    
+    func updateNSView(_ nsView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        nsView.load(request)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebView
+        
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+        
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            print("WebView finished loading")
+        }
+        
+        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+            print("WebView failed to load: \(error.localizedDescription)")
+        }
+    }
+}
 
 @main
-struct SwiftChatApp: App {
+struct ChatGPTMenuBarApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
     }
 }
